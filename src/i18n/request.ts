baba7dd54d5602fc94327b7bce35c2f0d2ce0150
NIBCarl <carlsteinborja3@gmail.com>
2025-08-23 23:00:19 +0800

    fixes error

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
-import { locales, type Locale } from '../middleware';
+import { locales, type Locale } from '../locales';
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
